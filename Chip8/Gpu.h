e662afa7817917f08219d250c9375a8a434518ac
Filoges <61270492+Filoges@users.noreply.github.com>
2020-02-20 14:33:46 +0100

    gesso

--- Chip8/Gpu.h
+++ Chip8/Gpu.h
@@ -17,10 +17,101 @@ public:
 		std::fill(frame_buffer.begin(), frame_buffer.end(), 0);
 	}
 
+
 	//settare pixel generico
-	void PixelSey(x, y, 1) {
-		frame_buffer[x]
+	void PixelSet(int x, int  y, bool choise) {
+
+		frame_buffer[y * 64 + x] = choise;
 	}
 
+
 	//ingresso un array di byte e la sua dim.
+	void DrawSprite(int x, int y, BYTE array[], int size) {
+
+		int N = size / 8; //height of the sprite
+
+		if (x + 8 <= 64 && y+N <= 32) {
+
+			for (int i = x; i <= x + 8; i++) {
+				for (int j = y; i <= y + N; j++) {
+
+					PixelSet(i, j, 0);
+
+				}
+
+			}
+		}
+
+		else if (x + 8 > 64 && y + N <= 32) {
+
+			int temp = x + 8 - 64;
+
+			for (int i = x; i <= x + 8- temp; i++) {
+				for (int j = y; i <= y + N; j++) {
+
+					PixelSet(i, j, 0);
+
+				}
+
+			}
+
+			for (int i = 0; i <= temp; i++) {
+				for (int j = y; i <= y + N; j++) {
+
+					PixelSet(i, j, 0);
+
+				}
+
+			}
+		}
+
+		else if (x + 8 <= 64 && y + N > 32) {
+
+			int temp = y + N - 32;
+
+			for (int i = x; i <= x + 8; i++) {
+				for (int j = y; i <= y + N - temp; j++) {
+
+					PixelSet(i, j, 0);
+
+				}
+
+			}
+
+			for (int i = x; i <= x + 8; i++) {
+				for (int j = 0; i <= temp; j++) {
+
+					PixelSet(i, j, 0);
+
+				}
+
+			}
+		}
+
+		else if (x + 8 > 64 && y + N > 32) {
+
+			int temp_x = x + 8 - 64;
+			int temp_y = y + N - 32;
+
+			for (int i = x; i <= x + 8 - temp_x; i++) {
+				for (int j = y; i <= y + N - temp_y; j++) {
+
+					PixelSet(i, j, 0);
+
+				}
+
+			}
+
+			for (int i = 0; i <= temp_x; i++) {
+				for (int j = 0; i <= temp_y; j++) {
+
+					PixelSet(i, j, 0);
+
+				}
+
+			}
+		}
+
+	}
+
 };
